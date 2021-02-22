31b095d13da6f006ecb815274348ff2b50b60fa9
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2021-02-22 07:10:21 +0100

    Setting Attributes 1

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -3,11 +3,13 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    imageUrl: "https://images.unsplash.com/photo-1613831242485-ac804daca659?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
   };
 
   render() {
     return (
       <React.Fragment>
+        <img src={this.state.imageUrl} alt="" />
         <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
