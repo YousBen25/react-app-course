cae6fd28c6b350dc836fdd58111873d0cfb2a689
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2021-02-23 08:52:08 +0100

    Passing children

--- src/Components/counters.jsx
+++ src/Components/counters.jsx
@@ -14,7 +14,9 @@ class Counters extends Component {
     return (
       <div>
         {this.state.counters.map(counter => 
-            <Counter key={counter.id} value={counter.value} />)}
+            <Counter key={counter.id} value={counter.value}>
+                <h2>Counter #{counter.id}</h2>
+            </Counter>)}
       </div>
     );
   }
