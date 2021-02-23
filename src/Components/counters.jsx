fe4475a4e35671f3ac086ff8bafba6ceac06e855
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2021-02-23 13:54:25 +0100

    Single source of truth

--- src/Components/counters.jsx
+++ src/Components/counters.jsx
@@ -10,15 +10,25 @@ class Counters extends Component {
       { id: 4, value: 0 },
     ],
   };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter(c => c.id !== counterId);
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
     this.setState({ counters });
   };
-  
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
+        <button onClick={this.handleReset} className="btn btn-success btn-lg">
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
